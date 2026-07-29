d4239d1f429819918e81b7b2d18eb7a33bab4a39
shubhamrangdal2001-hash <shubhamrangdal2001@gmail.com>
2026-07-29 12:31:24 +0530

    CICD run 6

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -14,26 +14,26 @@ from src.logger import logging
 # # Below code block is for production use
 # # -------------------------------------------------------------------------------------
 # # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "shubhamrangdal2000'"
+repo_name = "MLOPS_CAPSTONE_Project"
 
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # # -------------------------------------------------------------------------------------
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/shubhamrangdal2000/MLOPS_CAPSTONE_Project.mlflow/')
-dagshub.init(repo_owner='shubhamrangdal2000', repo_name='MLOPS_CAPSTONE_Project', mlflow=True)
-# -------------------------------------------------------------------------------------
+# mlflow.set_tracking_uri('https://dagshub.com/shubhamrangdal2000/MLOPS_CAPSTONE_Project.mlflow/')
+# dagshub.init(repo_owner='shubhamrangdal2000', repo_name='MLOPS_CAPSTONE_Project', mlflow=True)
+# # -------------------------------------------------------------------------------------
 
 
 def load_model(file_path: str):
